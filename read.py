9a949b496adb8860d42efca38e05538f4bfd3454
anaranjoingecall <anaranjo@ingecall.com>
2021-03-21 01:00:49 -0500

    parametros de pd.read_csv

--- read.py
+++ read.py
@@ -9,37 +9,42 @@
 get_ipython().run_line_magic('config', 'IPCompleter.greedy=True')
 
 
-# In[17]:
+# In[57]:
 
 
 import pandas as pd
+import numpy as np
 import IPython
+import os
 print(IPython.sys_info())
 
 
-# In[20]:
+# In[70]:
 
 
 get_ipython().run_line_magic('cd', "'/home/jovyan/python/dataset'")
 
 
-# In[21]:
+# In[71]:
 
 
 get_ipython().system('ls')
 
 
-# In[23]:
+# In[65]:
 
 
 dir = '/home/jovyan/python/dataset/{}'.format('data.csv')
-data = pd.read_csv(dir)
+file = 'data.csv'
+fullpath = os.path.join(dir, file)
+data = pd.read_csv(dir, sep = ",", dtype={"pclass": np.float64}, header=0, names=None, index_col=None, na_filter=False)
 
 
-# In[24]:
+# In[68]:
 
 
 data.head()
+data.columns.values
 
 
 # In[ ]:
