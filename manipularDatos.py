1e07dc2f97f43e5cd4f7cbd9fa055c8b265c3fb4
anaranjoingecall <anaranjo@ingecall.com>
2021-03-21 12:27:51 -0500

    cómputo de los valores devueltos

--- manipularDatos.py
+++ manipularDatos.py
@@ -15,28 +15,197 @@ import pandas as pd
 get_ipython().run_line_magic('cd', "'/home/jovyan/python/dataset'")
 
 
-# In[13]:
+# In[14]:
 
 
 ls
 
 
-# In[8]:
+# In[15]:
 
 
-dir = '/home/jovyan/python/dataset/{}'.format('data.csv')
+dir = '/home/jovyan/python/dataset/{}'.format('data3.csv')
 
 
-# In[9]:
+# In[16]:
 
 
 data = pd.read_csv(dir)
 
 
-# In[10]:
+# In[19]:
 
 
-data.head()
+data.head(10)
+
+
+# In[24]:
+
+
+urldata = "https://raw.githubusercontent.com/afnarqui/python/dev/dataset/data3.csv"
+data2 = pd.read_csv(urldata)
+
+
+# In[29]:
+
+
+data.tail()
+
+
+# In[26]:
+
+
+data2.head(3)
+
+
+# In[28]:
+
+
+data.shape
+
+
+# In[30]:
+
+
+data.columns.values
+
+
+# # Resumen estadísticos básicos con las varialbes numéricas.
+
+# In[32]:
+
+
+data.describe()
+
+
+# In[33]:
+
+
+data.dtypes
+
+
+# In[34]:
+
+
+pd.isnull(data["body"])
+
+
+# In[35]:
+
+
+pd.notnull(data["body"])
+
+
+# In[37]:
+
+
+pd.isnull(data["body"]).values.ravel().sum()
+
+
+# In[38]:
+
+
+pd.notnull(data["body"]).values.ravel().sum()
+
+
+# Los valores que faltan en un data set pueden venir por dos razones:
+# * Extracción de los datos
+# * Recolección de los datos
+
+# #### Borrado de valores que faltan
+
+# In[40]:
+
+
+data.dropna(axis=0, how="all")
+
+
+# In[41]:
+
+
+data3 = data
+
+
+# In[42]:
+
+
+data3.dropna(axis=0, how="any")
+
+
+# #### Cómputo de los valores faltantes
+
+# In[43]:
+
+
+data3 = data
+
+
+# In[44]:
+
+
+data3.fillna(0)
+
+
+# In[45]:
+
+
+data4 = data
+
+
+# In[46]:
+
+
+data4.fillna('Desconocido')
+
+
+# In[47]:
+
+
+data5 = data
+
+
+# In[55]:
+
+
+data5["body"] = data5["body"].fillna(0)
+data5["home.dest"] = data5["home.dest"].fillna("Desconocido")
+pd.isnull(data5["age"]).values.ravel().sum()
+
+
+# In[56]:
+
+
+data5["age"].fillna(data["age"].mean())
+
+
+# In[57]:
+
+
+pd.isnull(data5["age"]).values.ravel().sum()
+
+
+# In[58]:
+
+
+data5["age"] = data5["age"].fillna(data["age"].mean())
+
+
+# In[59]:
+
+
+pd.isnull(data5["age"]).values.ravel().sum()
+
+
+# In[60]:
+
+
+data5["age"][1291]
+
+
+# In[63]:
+
+
+data5["age"].fillna(method="ffill")
 
 
 # In[ ]:
