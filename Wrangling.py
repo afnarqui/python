51d03c11ff27aa6d93d61fb622155c3e76c7fd49
anaranjoingecall <anaranjo@ingecall.com>
2021-03-23 22:54:10 -0500

    filtros

--- Wrangling.py
+++ Wrangling.py
@@ -111,6 +111,83 @@ subset = data[sublist]
 subset.head()
 
 
+# In[23]:
+
+
+data[1:25]
+
+
+# In[24]:
+
+
+data[10:35]
+
+
+# In[26]:
+
+
+data[:8] # es lo mismo que data[1:8]
+
+
+# In[27]:
+
+
+data[60:]
+
+
+# In[29]:
+
+
+data[3330:]
+
+
+# In[33]:
+
+
+## Usuarios con Total Mins > 300
+data1 = data[data["Day Mins"] > 300]
+data1
+
+
+# In[34]:
+
+
+## Usuarios de Nueva york (State = "NY")
+data2 = data[data["State"]=="NY"]
+data2
+
+
+# In[38]:
+
+
+##AND -> &
+data3 = data[(data["State"]=="NY") & (data["Day Mins"]> 300)]
+data3.shape
+
+
+# In[41]:
+
+
+##OR -> |
+data4 = data[(data["Day Mins"]>300) | (data["State"]=="NY")]
+data4.shape
+data4.head()
+
+
+# In[42]:
+
+
+data5 = data[data["Day Calls"]< data["Night Calls"]]
+data5.shape
+
+
+# In[43]:
+
+
+data6 = data[data["Day Mins"]< data["Night Mins"]]
+data6.shape
+
+
 # In[ ]:
 
 
