cd7b84fb1c41ae7b732b3b27afeaf33d7c9d7536
anaranjoingecall <anaranjo@ingecall.com>
2021-03-30 10:44:11 -0500

    se crea la función montecarlo

--- pi.py
+++ pi.py
@@ -20,28 +20,36 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # * Calculamos el promedio de los n experimentos anteriores para dar un valor final de π.
 # 
 
-# In[3]:
+# In[8]:
 
 
-pi_avg = 0
-n = 1000
-pi_value_list = []
-for i in range(100):
-    value = 0
-    x = np.random.uniform(0,1,n).tolist()
-    y = np.random.uniform(0,1,n).tolist()
-    for j in range(n):
-        z = np.sqrt(x[j] * x[j] + y[j] * y[j])
-        if z <=1:
-            value +=1
-    float_value = float(value)
-    pi_value = float_value * 4 / n
-    pi_value_list.append(pi_value)
-    pi_avg += pi_value
-pi = pi_avg /100
+def pi_montecarlo(n, n_exp):
+    pi_avg = 0
 
-print(pi)
-plt.plot(pi_value_list)
+    pi_value_list = []
+    for i in range(n_exp):
+        value = 0
+        x = np.random.uniform(0,1,n).tolist()
+        y = np.random.uniform(0,1,n).tolist()
+        for j in range(n):
+            z = np.sqrt(x[j] * x[j] + y[j] * y[j])
+            if z <=1:
+                value +=1
+        float_value = float(value)
+        pi_value = float_value * 4 / n
+        pi_value_list.append(pi_value)
+        pi_avg += pi_value
+    pi = pi_avg /n_exp
+
+    print(pi)
+    fig = plt.plot(pi_value_list)
+    return (pi, fig)
+
+
+# In[10]:
+
+
+pi_montecarlo(10000, 200)
 
 
 # In[ ]:
