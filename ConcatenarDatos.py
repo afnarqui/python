e8b82b4c3969ba55e06aa601e74eef763384b057
anaranjoingecall <anaranjo@ingecall.com>
2021-04-02 11:57:17 -0500

    Datos distribuidos

--- ConcatenarDatos.py
+++ ConcatenarDatos.py
@@ -9,7 +9,7 @@
 import pandas as pd
 
 
-# In[17]:
+# In[31]:
 
 
 get_ipython().run_line_magic('cd', "'/home/jovyan/python/dataset'")
@@ -107,6 +107,85 @@ wine_scramble
 
 wine_scramble = pd.concat([data2, data1, data3], axis=0)
 wine_scramble
+
+
+# ### Datos distribuidos
+
+# In[32]:
+
+
+dir_distributed = '/home/jovyan/python/dataset/distributed-data/{}'.format('001.csv')
+data = pd.read_csv(dir_distributed)
+
+
+# In[33]:
+
+
+data.head()
+
+
+# In[34]:
+
+
+data.shape
+
+
+# * Importar el primer fichero
+# * Hacemos un bucle para ir recorriendo todos y cada uno de los ficheros. 
+#      * Importante tener una consistencia en el nombre de los ficheros
+#      * Importamos los ficheros uno a uno
+#      * Cada uno de ellos debe apendizarse (añadirse al final) del primer fichero que ya habíamos cargado
+# * Repetimos el bucle hasta que no queden ficheros
+
+# In[47]:
+
+
+filepath = '/home/jovyan/python/dataset/distributed-data/'
+data = pd.read_csv(filepath + '001.csv')
+final_length = len(data)
+for i in range(2, 333):
+    if i < 10:
+        filename = "00"+str(i)
+    if 10 <= i < 100:
+        filename = "0"+str(i)
+    if i >= 100:
+        filename = str(i)
+    file = filepath + filename + ".csv"
+    temp_data = pd.read_csv(file)
+    final_length += len(temp_data)
+    
+    data = pd.concat([ data, temp_data], axis= 0)
+    
+
+
+# In[44]:
+
+
+data.shape
+
+
+# In[45]:
+
+
+data.tail()
+
+
+# In[46]:
+
+
+data.head()
+
+
+# In[48]:
+
+
+final_length
+
+
+# In[50]:
+
+
+final_length == data.shape[0]
 
 
 # In[ ]:
